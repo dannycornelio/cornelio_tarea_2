2e8d24b5c69fcde3d936e17d833706e990c73629
dannycornelio <licdannycornelio@gmail.com>
2023-07-09 15:49:48 -0600

    creacion del gormulario y del JS

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manipulacion del DOM</title>
+</head>
+<body>
+    <h1>MANIPULACION DEL DOM EN JS</h1>
+    <form action="" id="form">
+        <input type="email" id="imail-form" placeholder="Ingrese un correo">
+        <input type="text" id="text-form" placeholder="Ingrese su nombre">
+        <input type="submit" id="submit-form" placeholder="Suscribirse">
+    </form>
+   
+   
+   
+    <script src="app.js"></script>
+</body>
+</html>
