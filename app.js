d6c8ca771dcb0e391dd00d3c4c1e34daecc9a51a
dannycornelio <licdannycornelio@gmail.com>
2023-07-09 19:44:27 -0600

    agregamos un elnace de google

--- app.js
+++ app.js
@@ -30,10 +30,19 @@ let button = document.querySelector("#button-form");
 //aqui modifico el el texto
 button.textContent = "ENVIAR AHORA";
 
+//alamcenamos el elemento a dentro de esta variable para llamarla por ese nombre
+enlaceElement = document.createElement('a');
+
+//agregamos un link de google
+enlaceElement.href = 'https://www.google.com';
+enlaceElement.textContent = 'PULSE AQUI PARA BUSQUEDAS';
+
+//agregamos el nodo hijo
+bodyElement.appendChild(enlaceElement);
+
 //agregamos un nuevo elemento hijo al body dentro de esta variable
 let Parrafo = document.createElement("p");
 //aqui con el textcontect agregamos lo que queremos agregar dentro del p
-Parrafo.textContent =
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo illo sed nemo autem facere! Autem molestiae cumque magnam odio voluptatum necessitatibus eos eligendi tempora ducimus. Necessitatibus consequuntur blanditiis assumenda eaque.";
+Parrafo.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo illo sed nemo autem facere! Autem molestiae cumque magnam odio voluptatum necessitatibus eos eligendi tempora ducimus. Necessitatibus consequuntur blanditiis assumenda eaque.";
 //para añadir el párrafo como hijo del elemento
 bodyElement.appendChild(Parrafo);
